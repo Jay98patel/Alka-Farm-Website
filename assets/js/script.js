c0445d1ab4db58619c68dff7eb2b7b72a04228cd
jay98patel <itsj10.jey@gmail.com>
2021-09-07 16:40:28 +0530

    implemented home section

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,30 +8,70 @@ speech.pitch = 1;
 window.speechSynthesis.speak(speech); */
 
 //menu toggle 
-const showMenu = (toggleId, navId) =>{
+const showMenu = (toggleId, navId) => {
     const toggle = document.getElementById(toggleId),
-    nav = document.getElementById(navId);
-    
+        nav = document.getElementById(navId);
+
     // Validate that variables exist
-    if(toggle && nav){
-        toggle.addEventListener('click', ()=>{
+    if (toggle && nav) {
+        toggle.addEventListener('click', () => {
             // We add the show-menu class to the div tag with the nav__menu class
             nav.classList.toggle('show-menu')
         })
     }
 }
-showMenu('nav-toggle','nav-menu');
+showMenu('nav-toggle', 'nav-menu');
 
 // remove menu mobile
 
-const navLink=document.querySelectorAll('.nav__link');
+const navLink = document.querySelectorAll('.nav__link');
 
-function linkAction(){
-    const navMenu=document.getElementById('nav-menu');
+function linkAction() {
+    const navMenu = document.getElementById('nav-menu');
     navMenu.classList.remove('show-menu');
-    console.log("caaled")
 }
 
-navLink.forEach(n=>{
-    n.addEventListener('click',linkAction);
+navLink.forEach(n => {
+    n.addEventListener('click', linkAction);
 });
+
+
+
+//==================== SCROLL SECTIONS ACTIVE LINK ====================
+const sections = document.querySelectorAll('section[id]')
+
+function scrollActive(){
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight;
+        const sectionTop = current.offsetTop - 50;
+        sectionId = current.getAttribute('id');
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
+        }else{
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive)
+
+//=================== change header ===================
+
+function scrollHeader() {
+    const nav = document.getElementById('header');
+    scrollY >= 200 ? nav.classList.add('scroll-header') : nav.classList.remove('scroll-header');
+
+}
+
+window.addEventListener('scroll',scrollHeader)
+
+
+/*==================== SHOW SCROLL TOP ====================*/ 
+function scrollTop(){
+    const scrollTop = document.getElementById('scroll-top');
+    if(this.scrollY >= 220) scrollTop.classList.add('show-scroll'); else scrollTop.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollTop)
+
